3bf1e97349eddbe93384a58e873453264a04a3ed
vishalsanfran <vkhawarey@maprtech.com>
2017-08-24 06:09:53 -0700

    rails g model User name:string email string && rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815064638) do
-
-  create_table "microposts", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20170824060746) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
