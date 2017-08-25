36fed3c26e02347bf0bd0b75ab1519e1971f6f2c
vishalsanfran <vkhawarey@maprtech.com>
2017-08-25 14:40:00 -0700

    login with tmp session and redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:sessions][:email].downcase)
     if user && user.authenticate(params[:sessions][:password])
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/ password combination'
       render 'new'
