ead83a0233f4349b957d0df2fab5ab7bb3c11e8f
vishalsanfran <vkhawarey@maprtech.com>
2017-08-25 11:26:27 -0700

    puma worker settings

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,4 +13,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
+  test "valid signup info" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Test user",
+                                         email: "test@example.com",
+                                         password: "password",
+                                         password_confirmation: "password" }}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
 end
