c264ad0f5740a61fd14bbcba3afc04c887226df4
vishalsanfran <vkhawarey@maprtech.com>
2017-08-25 13:30:49 -0700

    params has sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,10 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(email: params[:sessions][:email].downcase)
+    if user && user.authenticate(params[:sessions][:password])
     else
-      flash[:danger] = 'Invalid email/ password combination'
+      flash.now[:danger] = 'Invalid email/ password combination'
       render 'new'
     end
   end
