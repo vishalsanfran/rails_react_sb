f454ca62fcb2d8afa350c941629f103103be1dc5
vishalsanfran <vkhawarey@maprtech.com>
2017-08-25 15:06:30 -0700

    render current user links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,8 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-    user = User.find_by(email: params[:sessions][:email].downcase)
-    if user && user.authenticate(params[:sessions][:password])
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to user
     else
