b6ec6da669e581c61439824b52362bf2c490f5a0
vishalsanfran <vkhawarey@maprtech.com>
2017-08-25 13:20:01 -0700

    user.authenticate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   #get 'users/new'
 
   root 'static_pages#home'
@@ -6,7 +8,10 @@ Rails.application.routes.draw do
   get  '/about', to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup', to: 'users#new'
-  post 'signup', to: 'users#create'
+  post '/signup', to: 'users#create'
   resources :users
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
