2227ceb4890c356fba17d09412538f61bac65db1
vishalsanfran <vkhawarey@maprtech.com>
2017-08-25 15:46:06 -0700

    delete session and logout

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,7 +13,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
-  test "valid signup info" do
+  test "valid signup info followed by logout" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name: "Test user",
@@ -24,5 +24,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
     assert_not flash.empty?
+    assert is_logged_in?
   end
 end
